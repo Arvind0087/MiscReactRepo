91e33c6ce12246852d22d61067fba554f41cf079
ARVIND KUMAR <AK23612@Satya-Delhi.com>
2024-02-19 18:28:11 +0530

    form validation

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import GalleryPagination from "./components/gallerypagination/GalleryPagination"
 import MultipleSelectCheckbox from "./components/checkbox/MultipleSelectCheckbox";
 import Multicheckbox2 from "./components/checkbox/Multicheckbox2";
 import OrderTable from "./components/TablePaginationWithMultiCheckSelect/OrderTable";
+import MaterialUiForm from "./components/formikYupFormValidation/muiFormValidation1/MaterialUiForm";
 
 function App() {
   return (
     <>
       {/*<RadioButton />*/}
-      <GalleryPagination />
+      {/*<GalleryPagination /> */}
       {/*<MultipleSelectCheckbox />*/}
       {/*<Multicheckbox2 />*/}
       {/*<OrderTable /> */}
+      <MaterialUiForm />
     </>
   );
 }
